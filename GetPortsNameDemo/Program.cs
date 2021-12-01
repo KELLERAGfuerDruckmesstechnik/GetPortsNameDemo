9cfaba5061a23603bb7650b5c536fc7310d9b085
cBashTn <engineering@keller-druck.ch>
2021-12-01 12:49:30 +0100

    Added simple .NET 4.6.1 tool to check ports name

---
+++ GetPortsNameDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace GetPortsNameDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Suche nach existierenden Port-Namen...");
+            try
+            {
+                var portNames = SerialPort.GetPortNames();
+                Console.WriteLine("... Gefunden wurden " + portNames.Length + " Ports: ");
+
+                foreach (string portName in portNames)
+                {
+                    Console.WriteLine(" - '" + portName + "'");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("FEHLER: " + e);
+            }
+            Console.WriteLine("Ende des Programms. Bitte diesen Text kopieren und per Mail and KELLER senden!");
+            Console.ReadLine();
+        }
+    }
+}
